0b87b61bcb53ed2ed512061c5c1db407696ba9ac
Shreya Porwal <porwalshreya11111@gmail.com>
2025-11-09 05:09:50 +0530

    config for github

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import first_component from './components/first_component.vue';
+</script>
+
+<template>
+  <first_component/>
+</template>
+
+<style scoped></style>
---
+++ src/components/first_component.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+    <h1>My First App</h1>
+</template>
